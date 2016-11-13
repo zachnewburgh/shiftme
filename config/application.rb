d1b27ce2db626a3d2a23683e498d380d9abdb361
zachnewburgh <zach.newburgh@gmail.com>
2016-11-13 18:26:43 -0500

    Removing dotenv

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
-HOSTNAME = ENV['HOSTNAME']
+# HOSTNAME = ENV['HOSTNAME']
 
 module Shiftme
   class Application < Rails::Application
