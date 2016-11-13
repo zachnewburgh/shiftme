a818b0c45ab15b71dd06e1e455c9568ef1b3e65e
zachnewburgh <zach.newburgh@gmail.com>
2016-11-13 16:04:39 -0500

    Added first_name and last_name attributes

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 20161113200651) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "position"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
