025520a3c0bb113222411a9a3ffed41635402485
zachnewburgh <zach.newburgh@gmail.com>
2016-11-13 15:14:03 -0500

    Generated UserController with CRUD

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'user/index'
+
+  get 'user/new'
+
+  get 'user/show'
+
+  get 'user/update'
+
+  get 'user/destroy'
+
+  root 'application#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
