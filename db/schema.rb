29de4c41e96cb92fbc6c108df6c0f363331c4339
zachnewburgh <zach.newburgh@gmail.com>
2016-11-13 16:07:54 -0500

    Added provider and uid to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113200651) do
+ActiveRecord::Schema.define(version: 20161113210726) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20161113200651) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
